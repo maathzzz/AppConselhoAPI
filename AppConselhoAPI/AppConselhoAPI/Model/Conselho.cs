374c397235facc5620f8e9f0b371849533942c59
Matheus <matheusdeamorim20@gmail.com>
2022-10-18 12:25:42 -0300

    Adicionar arquivos de projeto.

---
+++ AppConselhoAPI/AppConselhoAPI/Model/Conselho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhoAPI.Model
+{
+    public class Conselho
+    {
+        public string id { get; set; }
+        public string advice { get; set; }
+
+        public Conselho()
+        {
+            this.id = " ";
+            this.advice = " ";
+        }
+    }
+}
---
+++ AppConselhoAPI/AppConselhoAPI/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using AppConselhoAPI.Model;
+using AppConselhoAPI.View;
+
+namespace AppConselhoAPI.Services
+{
+    internal class DataService
+    {
+    }
+}
